86a80b23e22d295b19a46ad93fe6984e2839ba20
Galan Rémi <remi.galan-alfonso@ensimag.grenoble-inp.fr>
2015-06-22 23:42:47 +0200

    git-rebase -i: add command "drop" to remove a commit
    Instead of removing a line to remove the commit, you can use the
    command "drop" (just like "pick" or "edit"). It has the same effect as
    deleting the line (removing the commit) except that you keep a visual
    trace of your actions, allowing a better control and reducing the
    possibility of removing a commit by mistake.
    
    Signed-off-by: Galan Rémi <remi.galan-alfonso@ensimag.grenoble-inp.fr>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t3404-rebase-interactive.sh
+++ t/t3404-rebase-interactive.sh
@@ -1102,4 +1102,20 @@ test_expect_success 'rebase -i commits that overwrite untracked files (no ff)' '
 	test $(git cat-file commit HEAD | sed -ne \$p) = I
 '
 
+test_rebase_end () {
+	test_when_finished "git checkout master &&
+	git branch -D $1 &&
+	test_might_fail git rebase --abort" &&
+	git checkout -b $1 master
+}
+
+test_expect_success 'drop' '
+	test_rebase_end dropTest &&
+	set_fake_editor &&
+	FAKE_LINES="1 drop 2 3 drop 4 5" git rebase -i --root &&
+	test E = $(git cat-file commit HEAD | sed -ne \$p) &&
+	test C = $(git cat-file commit HEAD^ | sed -ne \$p) &&
+	test A = $(git cat-file commit HEAD^^ | sed -ne \$p)
+'
+
 test_done
